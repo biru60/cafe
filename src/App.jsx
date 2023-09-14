ddb937e2379f97f7817889ba0f9fb7582ca3ace6
biru60 <u1904060@student.cuet.ac.bd>
2023-09-14 06:33:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Blog from './component/Blogs/Blog'
+import Header from './component/header/Header'
+
+function App() {
+ 
+
+  return (
+    <>
+   <Header></Header>
+   <Blog></Blog>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Blogs/Blog.jsx
@@ -0,0 +1,21 @@
+import { Children } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const Blog = () => {
+    const [blog,setBlog]=useState([]);
+    useEffect(()=>{
+     fetch('blog.json')
+     .then(res=>res.json())
+     .then(data=>setBlog(data))
+
+    },[])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blog;
